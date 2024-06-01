1ad7bd28070c4ef9e327aad4d23dd5143319366c
Yehor Popov <103197666+PopovYehor@users.noreply.github.com>
2024-06-01 19:01:16 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const port = process.env.PORT
+  const host = process.env.HOST
   const app = await NestFactory.create(AppModule);
-  await app.listen(port);
+  await app.listen(port, host);
 }
 bootstrap();
